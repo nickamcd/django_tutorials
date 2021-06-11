9d1b3698f1ab70e24c20cb206ba2b0d8246f0b2d
Nick McDaniel <nickamcd@gmail.com>
2021-06-11 12:06:49 -0700

    Added passing of context into templates

--- new_project/blog/views.py
+++ new_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+  {
+    'author': 'NickMcD', 
+    'title': 'Blog Post 1', 
+    'content': 'First post content',
+    'date_posted': 'June 11, 2021'
+  },
+  {
+    'author': 'JaneDoe', 
+    'title': 'Blog Post 2', 
+    'content': 'Second post content',
+    'date_posted': 'June 12, 2021'
+  },
+]
+
 # Create your views here.
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return render(request, 'blog/about.html')
+  return render(request, 'blog/about.html', {'title': 'About'})
