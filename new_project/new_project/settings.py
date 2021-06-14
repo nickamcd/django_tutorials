676d804deac430eb1b6f9ead16863386daf97179
Nick McDaniel <nickamcd@gmail.com>
2021-06-14 16:17:21 -0700

    Added password reset system that works via email

--- new_project/new_project/settings.py
+++ new_project/new_project/settings.py
@@ -139,3 +139,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Change default login urls
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+# Set up email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
