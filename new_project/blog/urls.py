efd1a3f08e507eb4e260bcd11c0096cad4dbd3c5
Nick McDaniel <nickamcd@gmail.com>
2021-06-14 13:28:14 -0700

    Made a list view for specific users posts

--- new_project/blog/urls.py
+++ new_project/blog/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
 from .views import (
-    PostListView, 
+    PostListView,
+    UserPostListView, 
     PostDetailView,
     PostCreateView,
     PostUpdateView,
@@ -12,6 +13,7 @@ from . import views
 urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
