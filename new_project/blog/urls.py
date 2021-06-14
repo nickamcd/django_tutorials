5f739d6ebf157ca6bfe2dddfc6b488179ca89708
Nick McDaniel <nickamcd@gmail.com>
2021-06-14 11:40:19 -0700

    Added functionality to update users own posts

--- new_project/blog/urls.py
+++ new_project/blog/urls.py
@@ -4,12 +4,14 @@ from .views import (
     PostListView, 
     PostDetailView,
     PostCreateView,
+    PostUpdateView,
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
