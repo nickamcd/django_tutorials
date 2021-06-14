8ed924e7dceed3f4748d7bb0829fa64e51bdb5ac
Nick McDaniel <nickamcd@gmail.com>
2021-06-14 10:46:59 -0700

    Added detail view for posts and link to detail view on home page

--- new_project/blog/views.py
+++ new_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -13,6 +13,10 @@ class PostListView(ListView):
   model = Post
   template_name = 'blog/home.html'
   context_object_name = 'posts'
+  ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+  model = Post
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
