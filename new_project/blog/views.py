44faba25e3b48213ab664c53a23af3861f2163b5
Nick McDaniel <nickamcd@gmail.com>
2021-06-14 11:30:21 -0700

    Added ability to create blog post and require login to do so

--- new_project/blog/views.py
+++ new_project/blog/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+  ListView, 
+  DetailView, 
+  CreateView
+)
 from .models import Post
 
 # Create your views here.
-def home(request):
-  context = {
-    'posts': Post.objects.all()
-  }
-  return render(request, 'blog/home.html', context)
-
 class PostListView(ListView):
   model = Post
   template_name = 'blog/home.html'
@@ -17,6 +16,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
   model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
