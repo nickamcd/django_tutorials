35e3313ccac51adda5c2d1f0def5e9d3c9760c89
Nick McDaniel <nickamcd@gmail.com>
2021-06-14 11:54:48 -0700

    Added delete function so that user can delete own post

--- new_project/blog/views.py
+++ new_project/blog/views.py
@@ -5,10 +5,22 @@ from django.views.generic import (
   DetailView, 
   CreateView,
   UpdateView,
+  DeleteView,
 )
 from .models import Post
 
 # Create your views here.
+
+# Create
+class PostCreateView(LoginRequiredMixin, CreateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+# Read
 class PostListView(ListView):
   model = Post
   template_name = 'blog/home.html'
@@ -18,14 +30,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
   model = Post
 
-class PostCreateView(LoginRequiredMixin, CreateView):
-  model = Post
-  fields = ['title', 'content']
-
-  def form_valid(self, form):
-    form.instance.author = self.request.user
-    return super().form_valid(form)
-
+# Update
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
   model = Post
   fields = ['title', 'content']
@@ -33,6 +38,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
   def form_valid(self, form):
     form.instance.author = self.request.user
     return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
+
+# Delete
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+  model = Post
+  success_url = '/'
 
   def test_func(self):
     post = self.get_object()
